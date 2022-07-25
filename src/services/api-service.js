090fb865e754dd2d11c8baaae370790105fc222b
Cesar Martinez <cmarvarc@hotmail.com>
2022-07-24 21:33:08 -0500

    Modified api fetch function to get only the same 15 results every time

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,7 +1,7 @@
 const BASE_URL = "https://randomuser.me/api/"
 
 export async function getUsers() {
-  const response = await fetch(`${BASE_URL}?results=15`, {
+  const response = await fetch(`${BASE_URL}?seed=foobar&results=15`, {
     method: "GET",
   })
   const data = await response.json();
